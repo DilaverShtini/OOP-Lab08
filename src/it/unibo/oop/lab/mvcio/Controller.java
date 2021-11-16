208ab6f6e88dcda8b798d651e60a40514dd681ff
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2021-11-16 16:18:10 +0100

    set a file as current file

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.io.File;
+
 /**
  * 
  */
@@ -12,15 +14,28 @@ public class Controller {
      * Implement this class with:
      * 
      * 1) A method for setting a File as current file
-     * 
-     * 2) A method for getting the current File
-     * 
-     * 3) A method for getting the path (in form of String) of the current File
-     * 
-     * 4) A method that gets a String as input and saves its content on the current
+     */ 
+    /*
+     * Set the file as current file 
+     */
+    private static final String HOME = System.getProperty("user.home"); 
+    private static final String SEPARATOR = System.getProperty("file.separator");
+    private static final String FILE = "output.txt";
+    private File file = null;
+    /**
+     *
+     */
+    public final void setCurrentFile() {
+       file = new File(HOME + SEPARATOR + FILE);
+    }
+    /* 2) A method for getting the current File
+     */
+    /* 3) A method for getting the path (in form of String) of the current File
+     */
+    /* 4) A method that gets a String as input and saves its content on the current
      * file. This method may throw an IOException.
-     * 
-     * 5) By default, the current file is "output.txt" inside the user home folder.
+     */
+    /* 5) By default, the current file is "output.txt" inside the user home folder.
      * A String representing the local user home folder can be accessed using
      * System.getProperty("user.home"). The separator symbol (/ on *nix, \ on
      * Windows) can be obtained as String through the method
