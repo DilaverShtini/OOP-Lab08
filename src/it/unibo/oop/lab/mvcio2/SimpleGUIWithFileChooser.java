5805470043bfd67ba216cb92fed989ff7e4b3019
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2021-11-18 21:35:47 +0100

    complete the action when the button is pressed

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -74,12 +74,6 @@ public final class SimpleGUIWithFileChooser {
                 }
             }
         }); 
-    /* 1) Add a JTextField and a button "Browse..." on the upper part of the
-     * graphical interface.
-     * Suggestion: use a second JPanel with a second BorderLayout, put the panel
-     * in the North of the main panel, put the text field in the center of the
-     * new panel and put the button in the line_end of the new panel.
-     */
         /*
          * added new panel
          */
@@ -96,19 +90,26 @@ public final class SimpleGUIWithFileChooser {
          */
         final JButton browseButton = new JButton("browse...");
         panel2.add(browseButton, BorderLayout.LINE_END);
+        browseButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                   try {
+                      final JFileChooser chooser = new JFileChooser();
+                      final int returnVal = chooser.showSaveDialog(frame);
+                      if (returnVal == JFileChooser.APPROVE_OPTION) {
+                          controller.setCurrentFile();
+                          controller.saveFile(textArea.getText());
+                      } else if (returnVal != JFileChooser.CANCEL_OPTION) {
+                          JOptionPane.showMessageDialog(frame, chooser);
+                      }
+                   } catch (Exception e1) {
+                       System.out.println(e1.toString());
+                   }
+            }
+        });
     }
-    /* 2) The JTextField should be non modifiable. And, should display the
-     * current selected file.
-     * 
-     * 3) On press, the button should open a JFileChooser. The program should
-     * use the method showSaveDialog() to display the file chooser, and if the
-     * result is equal to JFileChooser.APPROVE_OPTION the program should set as
-     * new file in the Controller the file chosen. If CANCEL_OPTION is returned,
-     * then the program should do nothing. Otherwise, a message dialog should be
-     * shown telling the user that an error has occurred (use
-     * JOptionPane.showMessageDialog()).
-     * 
-     * 4) When in the controller a new File is set, also the graphical interface
+     /* 4) When in the controller a new File is set, also the graphical interface
      * must reflect such change. Suggestion: do not force the controller to
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
