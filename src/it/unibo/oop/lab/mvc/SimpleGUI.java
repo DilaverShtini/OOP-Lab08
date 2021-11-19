3b3aecd2e570a1469874778bf20e9e4060ee8407
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2021-11-19 18:30:47 +0100

    completed SimpleGUI

--- src/it/unibo/oop/lab/mvc/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvc/SimpleGUI.java
@@ -1,9 +1,17 @@
 package it.unibo.oop.lab.mvc;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -36,8 +44,9 @@ public final class SimpleGUI {
 
     /**
      * builds a new {@link SimpleGUI}.
+     * @param ctrImpl
      */
-    public SimpleGUI() {
+    public SimpleGUI(final ControllerImpl ctrImpl) {
 
         /*
          * Make the frame half the resolution of the screen. This very method is
@@ -55,11 +64,76 @@ public final class SimpleGUI {
         frame.setSize(sw / 2, sh / 2);
 
         /*
+         * create panel 
+         */
+        final JPanel panel = new JPanel(new BorderLayout());
+        frame.setContentPane(panel);
+        /*
+         * create TextField 
+         */
+        final JTextField txtField = new JTextField();
+        panel.add(txtField, BorderLayout.NORTH);
+        /*
+         * create TextArea
+         */
+        final JTextArea txtArea = new JTextArea();
+        panel.add(txtArea, BorderLayout.CENTER);
+        /*
+         * create the new panel that contains two button
+         */
+        final JPanel panel2 = new JPanel(new FlowLayout());
+        frame.add(panel2, BorderLayout.SOUTH);
+        /*
+         * create two buttons and set their dimension
+         */
+        final JButton printButton = new JButton("Print");
+        final Dimension d = printButton.getPreferredSize();
+        printButton.setPreferredSize(new Dimension(d.width, d.height * 2));
+        final JButton historyButton = new JButton("Show history");
+        final Dimension d1 = historyButton.getPreferredSize();
+        historyButton.setPreferredSize(new Dimension(d1.width, d1.height * 2));
+        panel2.add(printButton, BorderLayout.SOUTH);
+        panel2.add(historyButton, BorderLayout.SOUTH);
+        /*
+         * close the program
+         */
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*
+         * when the button is pressed
+         */
+        printButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                ctrImpl.setNextString(txtField.getText());
+                ctrImpl.printString();
+            }
+
+        });
+
+        historyButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                txtArea.insert(ctrImpl.getHistory().toString(), 0);
+            }
+
+        });
+        /*
          * Instead of appearing at (0,0), upper left corner of the screen, this
          * flag makes the OS window manager take care of the default positioning
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+    }
+
+    public void display() {
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... args) {
+        final SimpleGUI simpleGui = new SimpleGUI(new ControllerImpl());
+        simpleGui.display();
     }
 
 }
